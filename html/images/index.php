2a2f1a0ff6995629df162a702bdf778b587e1822
sweng66 <sweng@stanford.edu>
2023-09-13 14:30:02 -0700

    adding images for 04* 05*

---
+++ html/images/index.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * index.php
+ *
+ * Blank file
+ * @author Florian Leitner <florian.leitner AT student DOT tugraz DOT at>
+ * @version 1.0
+ * @package YPLP
+ * @copyright (c) 2009 Yeast Genetics and Molecular Biology Group University Graz
+ */
+?>
